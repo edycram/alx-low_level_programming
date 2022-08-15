966440e152b0060d549cdc02e2fa9a2f5928e2bd
edycram <edycramu@gmail.com>
2022-08-15 11:40:35 -0700

    Task 4. Free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a linked list
+ * @head: listint_t list to be freed
+ */
+void free_listint2(listint_t **head)
+{
+	if (*head == NULL)
+		return;
+
+	while (*head)
+	{
+		free(*head);
+		*head = (*head)->next;
+	}
+
+	*head = NULL;
+}
