6a736be7c813acd9865611effc2e6cdcd98d7860
edycram <edycramu@gmail.com>
2022-08-16 10:23:14 -0700

    Task 12. Print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,8 +8,22 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	if (head == NULL)
-		exit(98);
+	size_t num = 0;
+	long int diff;
 
-	return (0);
+	while (head)
+	{
+		diff = head - head->next;
+		num++;
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (diff > 0)
+			head = head->next;
+		else
+		{
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			break;
+		}
+	}
+
+	return (num);
 }
